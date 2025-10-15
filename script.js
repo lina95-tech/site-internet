bc1ccf569a34d6e02ca5618bad1c410dfd33edb5
lina95-tech <linahachainane@yahoo.com>
2025-10-15 11:50:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Affiche un message de bienvenue quand la page charge
 window.addEventListener("load", () => {
-    alert("Bienvenue sur mon site !");
+    alert("Bienvenue sur mon site d'esthetiqye!");
 });
 
 // Change la couleur du titre quand on clique dessus
