12d32888f83fde266d369c2d2302845890571e49
lina95-tech <linahachainane@yahoo.com>
2025-12-10 12:03:57 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-
-// Change la couleur du titre quand on clique dessus
-const titre = document.querySelector("h1");
-titre.addEventListener("click", () => {
-    titre.style.color = "purple";
-    titre.textContent = "HUDA BEAUTY";
-})
